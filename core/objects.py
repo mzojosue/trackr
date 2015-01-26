815846efd1860e795a8cdd85455770968bd4c66f
dragonborn <dragonborn@new-host-7.home>
2015-01-25 20:36:01 -0500

    Further implemented task_widget object. Cleaned up some whitespace.

--- core/objects.py
+++ core/objects.py
@@ -4,193 +4,194 @@ today = datetime.today
 
 
 class Worker(object):
-    workers = {}
-    # pay rate constants
-    A_RATE = 100.84
-    A_RATE_journeyman = 97.38
-    B_RATE = 51.76
+	workers = {}
+	# pay rate constants
+	A_RATE = 100.84
+	A_RATE_journeyman = 97.38
+	B_RATE = 51.76
 
-    def __init__(self, name, phone=None, email=None, job=None, rate=None):
-        self.name = name
-        super(Worker, self).__setattr__('job', job)
-        self.prev_jobs = []
-        self.phone = str(phone)
-        self.email = str(email)
-        self.job = job
-        if rate is 'a':
-            self.rate = Worker.A_RATE
-        elif rate is 'b':
-            self.rate = Worker.B_RATE
+	def __init__(self, name, phone=None, email=None, job=None, rate=None):
+		self.name = name
+		super(Worker, self).__setattr__('job', job)
+		self.prev_jobs = []
+		self.phone = str(phone)
+		self.email = str(email)
+		self.job = job
+		if rate is 'a':
+			self.rate = Worker.A_RATE
+		elif rate is 'b':
+			self.rate = Worker.B_RATE
 
-    def __setattr__(self, name, value):
-        if name is 'job':
-            if self.job not in self.prev_jobs:
-                self.prev_jobs.append(self.job)
-        return super(Worker, self).__setattr__(name, value)
+	def __setattr__(self, name, value):
+		if name is 'job':
+			if self.job not in self.prev_jobs:
+				self.prev_jobs.append(self.job)
+		return super(Worker, self).__setattr__(name, value)
 
 
 class Job(object):
-    number = 0
-    jobs = {}
+	number = 0
+	jobs = {}
 
-    def __init__(self, name, number=None, po_pre=None,
-                 gc=None, foreman=None, sub_path=None,
-                 rate='a', projected_cost=None, tax_exempt=False):
-        ##TODO:implement better document storage
+	def __init__(self, name, number=None, po_pre=None,
+	             gc=None, foreman=None, sub_path=None,
+	             rate='a', projected_cost=None, tax_exempt=False):
+		##TODO:implement better document storage
 
-        self.name = '-'.join([str(number), str(name)])
-        if not number:
-            Job.number += 1
-            self.number = Job.number
-        else:
-            self.number = number
-        Job.jobs[self.number] = self
-        self.alt_name = ""
-        self.address = ""
-        self.gc = gc
-        self.foreman = foreman
-        self.workers = []
-        self.material_lists = []
-        self.po_pre = po_pre
-        self.tax_exempt = tax_exempt
-        self._PO = 0  # stores most recent PO suffix number
-        self.POs = {}  # stores PO strings as keys
-        self.projected_cost = projected_cost
+		self.name = '-'.join([str(number), str(name)])
+		if not number:
+			Job.number += 1
+			self.number = Job.number
+		else:
+			self.number = number
+		Job.jobs[self.number] = self
+		self.alt_name = ""
+		self.address = ""
+		self.gc = gc
+		self.foreman = foreman
+		self.workers = []
+		self.material_lists = []
+		self.po_pre = po_pre
+		self.tax_exempt = tax_exempt
+		self._PO = 0  # stores most recent PO suffix number
+		self.POs = {}  # stores PO strings as keys
+		self.projected_cost = projected_cost
+		self.tasks = {}
 
-        # Job.timesheets.key is datetime.datetime object
-        #Job.timesheets.value is [ 'pathname/to/timesheet', hours ]
-        self.timesheets = {}
+		# Job.timesheets.key is datetime.datetime object
+		# Job.timesheets.value is [ 'pathname/to/timesheet', hours ]
+		self.timesheets = {}
 
-        if rate is 'a':
-            self.rate = Worker.A_RATE
-        elif rate is 'b':
-            self.rate = Worker.B_RATE
-        self.start_date = None
-        self.sub_path = sub_path
+		if rate is 'a':
+			self.rate = Worker.A_RATE
+		elif rate is 'b':
+			self.rate = Worker.B_RATE
+		self.start_date = None
+		self.sub_path = sub_path
 
-    @property
-    def next_po(self):
-        _po = self._PO
-        self._PO += 1
-        return _po
+	@property
+	def next_po(self):
+		_po = self._PO
+		self._PO += 1
+		return _po
 
-    @staticmethod
-    def init_struct(self):
-        """ Initializes project directory hierarchy. """
-        ##TODO:initialize documents w/ job information
-        if self.sub_path:
-            return NotImplemented
+	@staticmethod
+	def init_struct(self):
+		""" Initializes project directory hierarchy. """
+		##TODO:initialize documents w/ job information
+		if self.sub_path:
+			return NotImplemented
 
-    @property
-    def path(self):
-        """ Return absolute sub path using global project path and Job.sub_path """
-        return NotImplemented
+	@property
+	def path(self):
+		""" Return absolute sub path using global project path and Job.sub_path """
+		return NotImplemented
 
-    @property
-    def labor(self):
-        """ Calculates labor totals """
-        hrs = 0.0
-        for i in self.timesheets.itervalues():
-            hrs += float(i[1])  # grab second item in list
-        return hrs
+	@property
+	def labor(self):
+		""" Calculates labor totals """
+		hrs = 0.0
+		for i in self.timesheets.itervalues():
+			hrs += float(i[1])  # grab second item in list
+		return hrs
 
-    @property
-    def cost(self):
-        """ Calculates job cost totals """
-        amt = 0.0
-        for i in self.timesheets.itervalues():
-            amt += (float(i[1]) * float(self.rate))
-        for i in self.POs.itervalues():
-            amt += i.quote.price
-        return amt
+	@property
+	def cost(self):
+		""" Calculates job cost totals """
+		amt = 0.0
+		for i in self.timesheets.itervalues():
+			amt += (float(i[1]) * float(self.rate))
+		for i in self.POs.itervalues():
+			amt += i.quote.price
+		return amt
 
 
 class MaterialList(object):
-    lists = []
+	lists = []
 
-    def __init__(self, job, items=None, foreman=None, date_sent=today(), date_due=None, comments="", doc=None):
-        job.material_lists.append(self)
-        self.doc = doc
-        self.job = job
-        self.items = items
-        self.foreman = foreman
-        self.date_sent = date_sent
-        self.date_due = date_due
-        self.comments = comments
-        self.quotes = []
-        self.todo = True
-        self.fulfilled = False
+	def __init__(self, job, items=None, foreman=None, date_sent=today(), date_due=None, comments="", doc=None):
+		job.material_lists.append(self)
+		self.doc = doc
+		self.job = job
+		self.items = items
+		self.foreman = foreman
+		self.date_sent = date_sent
+		self.date_due = date_due
+		self.comments = comments
+		self.quotes = []
+		self.todo = True
+		self.fulfilled = False
 
-    def issue_po(self, quote, fulfills=False):
-        return PO(self.job, quote=quote, fulfills=fulfills)
+	def issue_po(self, quote, fulfills=False):
+		return PO(self.job, quote=quote, fulfills=fulfills)
 
 
 class Quotes(object):
-    def __init__(self, mat_list, price=0.0, vend=None, doc=''):
-        self.mat_list = mat_list
-        self.mat_list.quotes.append(self)
-        self.price = float(price)
-        self.vend = str(vend)
-        self.doc = str(doc)  # document target path/name
+	def __init__(self, mat_list, price=0.0, vend=None, doc=''):
+		self.mat_list = mat_list
+		self.mat_list.quotes.append(self)
+		self.price = float(price)
+		self.vend = str(vend)
+		self.doc = str(doc)  # document target path/name
 
 
 class PO(object):
-    def __init__(self, job, date_issued=today(), fulfills=False,
-                 mat_list=None, quote=None, desc=None, deliveries=None):
-        num = job.nextPO()
-        self.name = '-'.join([str(job.name), str(num)])
-        self.job = job
-        self.mat_list = mat_list
-        if fulfills:
-            self.mat_list.fulfilled = True
-        self.quote = quote
-        self.job.POs[self.name] = self
-        self.date_issued = date_issued
-        self.deliveries = deliveries  # stores initial delivery date
-        self.backorders = None  # stores any backorder delivery dates
-        self.desc = str(desc)
+	def __init__(self, job, date_issued=today(), fulfills=False,
+	             mat_list=None, quote=None, desc=None, deliveries=None):
+		num = job.nextPO()
+		self.name = '-'.join([str(job.name), str(num)])
+		self.job = job
+		self.mat_list = mat_list
+		if fulfills:
+			self.mat_list.fulfilled = True
+		self.quote = quote
+		self.job.POs[self.name] = self
+		self.date_issued = date_issued
+		self.deliveries = deliveries  # stores initial delivery date
+		self.backorders = None  # stores any backorder delivery dates
+		self.desc = str(desc)
 
 
 class Vendor(object):
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
 
 class Delivery(object):
-    """ Represents a future delivery.
-    :param
-        po:     pointer to PO object
-    """
-    deliveries = []
+	""" Represents a future delivery.
+	:param
+		po:     pointer to PO object
+	"""
+	deliveries = []
 
-    def __init__(self, po, items=None, expected=None, destination='shop'):
-        Delivery.deliveries.append(self)
-        self.delivered = False
-        self.po = po
-        if items is None:
-            self.items = po.items
-        else:
-            self.items = items
-        self.expected = expected
-        self.destination = destination
+	def __init__(self, po, items=None, expected=None, destination='shop'):
+		Delivery.deliveries.append(self)
+		self.delivered = False
+		self.po = po
+		if items is None:
+			self.items = po.items
+		else:
+			self.items = items
+		self.expected = expected
+		self.destination = destination
 
 
 class Todo(object):
-    """ Represents tasks to-do
-    :param
-        name:   the name of the task
-        task:   text description of the task. may include link
-        due:    task due date
-        notif:  date/time to follow-up
-    """
-    todos = {}
-    completed = {}
+	""" Represents tasks to-do
+	:param
+		name:   the name of the task
+		task:   text description of the task. may include link
+		due:    task due date
+		notif:  date/time to follow-up
+	"""
+	todos = {}
+	completed = {}
 
-    def __init__(self, name, task="", due=None, notify=None):
-        self.name = str(name)
-        self.hash = hash(self.name)
-        self.hash = abs(self.hash)     # ensure positive values
-        Todo.todos[self.hash] = self
-        self.task = task
-        self.due = due
-        self.notify = notify
+	def __init__(self, name, task="", due=None, notify=None):
+		self.name = str(name)
+		self.hash = hash(self.name)
+		self.hash = abs(self.hash)  # ensure positive values
+		Todo.todos[self.hash] = self
+		self.task = task
+		self.due = due
+		self.notify = notify
--- core/page.py
+++ core/page.py
@@ -21,91 +21,93 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+	return '.' in filename and \
+	       filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 def upload_file(f):
-    try:
-        if f and allowed_file(f.filename):
-            filename = secure_filename(f.filename)
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return True
-    finally:
-        pass
-    return False
+	try:
+		if f and allowed_file(f.filename):
+			filename = secure_filename(f.filename)
+			f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return True
+	finally:
+		pass
+	return False
 
 
 @app.route('/')
 def root():
-    return redirect(url_for('home'))
+	return redirect(url_for('home'))
 
 
 @app.route('/home')
 def home():
-    jobs = Job.jobs
-    lists = MaterialList.lists
-    todos = Todo.todos.itervalues()
-    return render_template('dashboard.html', jobs=jobs, lists=lists, todos=todos)
+	jobs = Job.jobs
+	lists = MaterialList.lists
+	_todos = Todo.todos.itervalues()
+	_completed = Todo.completed.itervalues()
+	return render_template('dashboard.html', jobs=jobs, lists=lists, todos=_todos, completed=_completed)
 
 
 @app.route('/j/')
 def all_jobs():
-    return render_template('all_jobs.html')
+	return render_template('all_jobs.html')
 
 
 @app.route('/j/<job_name>')
 def show_job(job_name=None):
-    return render_template('job.html', job=job_name)
+	return render_template('job.html', job=job_name)
 
 
 @app.route('/j/create', methods=['GET', 'POST'])
 def create_job():
-    # not implemented yet
-    return NotImplemented
+	# not implemented yet
+	return NotImplemented
 
 
 @app.route('/material', methods=['GET', 'POST'])
 def material():
-    if request.method is 'POST':
-        f = request.files['list']
-        upload_file(f)
-        j = request.form['job']
-        j = Job.jobs[j]
-        due = request.form['shipping_date']
-        ##TODO:turn due into datetime obj
+	if request.method is 'POST':
+		f = request.files['list']
+		upload_file(f)
+		j = request.form['job']
+		j = Job.jobs[j]
+		due = request.form['shipping_date']
+		##TODO:turn due into datetime obj
 
-        ##TODO:pass intended job
-        MaterialList(j, doc=f.filename, date_due=due)
-        return "successfully uploaded"
-    else:
-        return render_template('material.html', jobs=Job.jobs)
+		##TODO:pass intended job
+		MaterialList(j, doc=f.filename, date_due=due)
+		return "successfully uploaded"
+	else:
+		return render_template('material.html', jobs=Job.jobs)
 
 
 @app.route('/quote', methods=['GET', 'POST'])
 def quote():
-    """ Used for uploading and associating a quote with a material list
-    :param:
-    :return:
-    """
-    if request.method is 'POST':
-        ##TODO:correctly implement document upload
-        f = request.files['quote']
-        upload_file(f)
-    else:
-        return render_template('delivery.html')
+	""" Used for uploading and associating a quote with a material list
+	:param:
+	:return:
+	"""
+	if request.method is 'POST':
+		##TODO:correctly implement document upload
+		f = request.files['quote']
+		upload_file(f)
+	else:
+		return render_template('delivery.html')
 
 
 @app.route('/task/new', methods=['POST'])
 def new_todo():
-    _title = request.form['title']
-    _task = request.form['task']
-    Todo(_title, task=_task)
-    return redirect(request.referrer)
+	_title = request.form['title']
+	_task = request.form['task']
+	Todo(_title, task=_task)
+	return redirect(request.referrer)
+
 
 @app.route('/task/<t_hash>/complete')
 def todo_complete(t_hash):
-    _todo = Todo.todos[int(t_hash)]
-    Todo.completed[_todo.hash] = _todo
-    del Todo.todos[_todo.hash]
-    return redirect(request.referrer)
+	_todo = Todo.todos[int(t_hash)]
+	Todo.completed[_todo.hash] = _todo
+	del Todo.todos[_todo.hash]
+	return redirect(request.referrer)
