afcb871bfc84ebeb1af61c043a720af45379f77e
Josue Figueroa <mzo.josue@gmail.com>
2015-03-23 14:34:13 -0400

    Trying to fix ENV_ROOT variable

--- core/__init__.py
+++ core/__init__.py
@@ -2,3 +2,20 @@ from objects import *
 from db import *
 from parsing import *
 from core.frontend.page import *
+import yaml
+
+global ENV_ROOT
+
+try:
+	_config  = 'config.yaml'
+	SETTINGS = yaml.load(open(_config))
+	ENV_ROOT = SETTINGS['data_root']
+	print "Loaded settings from '%s'" % (_config)
+except IOError:
+	ENV_ROOT = '//SERVER/Documents/Esposito'
+	print "Unable to load settings. Using default root directory location"
+
+JOB_SUB_DIR = os.path.join(ENV_ROOT, 'Jobs')
+AwardedJob.default_sub_dir = JOB_SUB_DIR
+
+init_db()
--- trackr.py
+++ trackr.py
@@ -1,23 +1,4 @@
-import os
-import yaml
-
 import core
-
-global ENV_ROOT
-
-try:
-	_config  = 'config.yaml'
-	SETTINGS = yaml.load(open(_config))
-	ENV_ROOT = SETTINGS['data_root']
-	print "Loaded settings from '%s'" % (_config)
-except IOError:
-	ENV_ROOT = '//SERVER/Documents/Esposito'
-	print "Unable to load settings. Using default root directory location"
-
-JOB_SUB_DIR = os.path.join(ENV_ROOT, 'Jobs')
-core.AwardedJob.default_sub_dir = JOB_SUB_DIR
-
-core.init_db()
 
 if __name__ == "__main__":
 	core.app.run(host='0.0.0.0', port=8080, debug=True)
