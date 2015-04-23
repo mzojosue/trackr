a0b10bf24b416c664d1db470e3a91bdbd999e1cb
dragonborn <dragonborn@new-host-7.home>
2015-04-22 23:46:17 -0400

    DIRTY: Updated parsing.import_po_log to use objects from openpyxl

--- core/parsing.py
+++ core/parsing.py
@@ -2,6 +2,7 @@ from xlrd import open_workbook, xldate_as_tuple
 from xlwt import *
 from xlutils.copy import copy
 from parse import parse
+import openpyxl
 
 from datetime import datetime, date, timedelta
 import environment
@@ -30,37 +31,34 @@ def ensure_write(f, *args, **kwargs):
 	return try_write
 
 def import_po_log(create=False, poLog=environment.get_po_log):
-	log = open_workbook(poLog, on_demand=True)
-	_nsheet = log.nsheets - 2            # Omit last 2 pages ( Stock, Small Projects )
-	for _sheetNum in range(1, _nsheet):  # Omit first template page
-		_sheet = log.sheet_by_index(_sheetNum)
+	# TODO: add logger debugging hooks
+	log = openpyxl.load_workbook(poLog, read_only=True, guess_types=True)
+	_nsheet = len(log.get_sheet_names()) - 2
+	for _sheetNum in range(1, _nsheet):
+		_sheet = log.get_sheet_by_name(log.get_sheet_names()[_sheetNum])
 		if create:
 			try:
-				# TODO: dynamically check sheet name. do not continue import operation if sheet is not a jobs
-				_job = objects.AwardedJob(*[i for i in parse("{} - {}", _sheet.name)], sheet_num=_sheetNum)
+				_job = objects.AwardedJob(*[i for i in parse("{} - {}", _sheet.title)])
 			except TypeError:
-				pass    # Sheet name does not match regex
-		for _row in range(2, _sheet.nrows):
-			_row = _sheet.row_slice(_row)
+				pass                # sheet does not match regex
+		for _row in _sheet.rows:
 			__po = _row[0].value
-			if not __po:
-				# skips empty row by assuming that a row without a PO# is a row without content
+			__po = [i for i in parse("{:d}-{}-{:d}", __po)]
+			if len(__po) is not 3:
+				# skips empty row by assuming that a row
 				continue
+			# TODO: parse vendor cell and create vendor object
 			__vend = _row[1].value
 			__price = _row[2].value
-
 			if _row[3].value:
 				try:
-					__date_issued = date(*xldate_as_tuple(_row[3].value, log.datemode)[:3])
+					__date_issued = datetime.strptime(_row[3].value, '%m.%d.%Y').date()
 				except ValueError:
-					try:
-						__date_issued = datetime.strptime(_row[3].value, '%m.%d.%Y').date()
-					except ValueError:
-						__date_issued = datetime.strptime(_row[3].value, '%m.%d.%y').date()
-					except TypeError:
-						__d = [int(i) for i in parse("{}.{}.{}", _row[3].value)]
-						__date_issued = date(__d[2], __d[0], __d[1])
-						del __d
+					__date_issued = datetime.strptime(_row[3].value, '%m.%d.%y').date()
+				except TypeError:
+					__d = [int(i) for i in parse("{}.{}.{}", _row[3].value)]
+					__date_issued = date(__d[2], __d[0], __d[1])
+					del __d
 			else:
 				__date_issued = None
 
@@ -73,35 +71,32 @@ def import_po_log(create=False, poLog=environment.get_po_log):
 			except IndexError:
 				__comment = ""
 
-			# Create MaterialList objects
 			if create:
+				# Create MaterialList objects
 				if '\\' in __mat_list_val:
 					__mat_list_val = str(__mat_list_val).replace('\\', '/')
 					_mat_list = objects.MaterialList(job=_job, doc=objects.os.path.split(__mat_list_val), date_sent=__date_issued, task=False)
+
 				else:
 					_mat_list = objects.MaterialList(job=_job, items=__mat_list_val, date_sent=__date_issued, task=False)
 				_mat_list.sent_out = True
 				if _mat_list.age > 5:
 					_mat_list.delivered = True
 
-				# Create Quote objects
+				# Create MaterialListQuote objects
 				if '\\' in __quote_val:
 					_quote = objects.MaterialListQuote(mat_list=_mat_list, price=__price, vend=__vend, date_uploaded=__date_issued, doc=objects.os.path.split(__quote_val))
 				else:
 					_quote = objects.MaterialListQuote(mat_list=_mat_list, price=__price, vend=__vend, date_uploaded=__date_issued)
 
 				# Create PO objects
-				_pre = parse("{pre}-{:d}", __po)['pre']
-				_num = parse("{}-{num:d}", __po)['num']
-				if str(_pre) is not str(_job.po_pre):
-					po_pre = _pre
-				else: po_pre = None
-				_po = objects.PO(_job, _mat_list, __date_issued, _quote, desc=__comment, po_num=_num, po_pre=po_pre, update=False)
-
-				del _mat_list, _quote, _po
-			# Delete all variables between each iteration
-			del __po, __vend, __price, __date_issued, __mat_list_val, __quote_val, __comment
-	del log
+				_po_pre  = '-'.join(__po[:1])
+				_po_num  = __po[2]
+				if str(_po_pre) is not str(_job.po_pre):
+					_pre = _po_pre
+				else:
+					_pre = None
+				_po = objects.PO(_job, _mat_list, __date_issued, _quote, desc=__comment, po_num=_po_num, po_pre=_pre, update=False)
 
 
 def parse_job_info(jobInfo):
