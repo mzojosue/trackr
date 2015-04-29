adc094c274aa0d6807ec62a79eb53f9678a5e6c2
mzojosue <mzo.josue@gmail.com>
2015-04-29 16:01:03 -0400

    Revert "Trying to implement yaml functions"
    This reverts commit fb9a666fef6ba49eda3d28fd36849c07b20d64e8.

--- trackr.py
+++ trackr.py
@@ -3,4 +3,4 @@ import core
 if __name__ == "__main__":
 	core.scheduler.add_job(core.reset_db)
 	core.scheduler.start()
-	core.app.run(host='0.0.0.0', port=8080, debug=False)
+	core.app.run(host='0.0.0.0', port=8080, debug=True)
