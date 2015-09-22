6f5fd54e60c3c5b8205e61e1a5f5ee0cd030f979
sway figueroa <mzo.josue@gmail.com>
2015-09-22 00:47:32 -0400

    Updated AwardedJob app.route functions and associated templates

--- core/frontend/material_cycle.py
+++ core/frontend/material_cycle.py
@@ -53,7 +53,7 @@ def job_materials(job_num=None):
 				_label = request.form['listLabel']
 				__obj = MaterialList(_job, items=__items, date_due=_date_due, label=_label, user=auth)
 			return redirect(url_for('material_list', m_hash=__obj.hash))
-		return render_template('jobs/job_materials.html', job=_job)
+		return render_template('jobs/job_materials.html', job=_job, usr=auth)
 	except KeyError:
 		flash('Error! AwardedJob does not exist.', 'danger')
 		return redirect(request.referrer)
